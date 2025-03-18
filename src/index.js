e5d0ed7cd8374d72ea76948d9e6eb2180d35481d
malbuquerque2 <malbuquerque@opensoft.pt>
2025-03-18 14:46:01 +0000

    forma reduzida js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; //Import correto para react 18
-import Primeiro from './components/basicos/Primeiro';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import ComParametros from './components/basicos/ComParametros';
-import Fragmento from './components/basicos/Fragmento';
-//react 18 removou essa forma de fazer a renderização
-// ReactDOM.render("Olá, mundo!", document.getElementById('root'));
+import App from './App';
 
-
-ReactDOM.createRoot(document.getElementById('root'))
-    .render(
-        <div id='app'>
-            <Primeiro />
-            <ComParametros titulo="Esse é o título" aluno="Mario" nota={2} />
-            <Fragmento />
-        </div>); //renderizando o elemento criado acima
-
+ReactDOM.createRoot(document.getElementById('root')).render(<App /> ); 
